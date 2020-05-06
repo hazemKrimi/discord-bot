d0b635ade35c0b6e4a2988914b0312c2a71814ab
Hazem Krimi <krimihazem1@gmail.com>
2020-05-06 11:59:30 +0100

    improved the queue system

--- commands/music/play.js
+++ commands/music/play.js
@@ -34,8 +34,6 @@ module.exports = class Play extends Command {
 
             if (!voiceChannel) return message.reply('you need to join a channel!');
 
-            // TODO change if to switch
-
             if (query.match(/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/\S+/)) {
                 const link = query.match(/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/\S+/)[0];
                 const id = link.replace(/(>|<)/gi, '').split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/)[2].split(/[^0-9a-z_\-]/i)[0];
@@ -72,16 +70,33 @@ module.exports = class Play extends Command {
                 page.setDefaultNavigationTimeout(0);
                 page.setDefaultTimeout(0);
                 await page.goto(link, { waitUntil: 'networkidle2' });
+                const titleHandle = await page.$('title');
                 const metaHandle = await page.$('meta[property="og:video:url"]');
+                const durationString = (await page.content()).match(/mediaPresentationDuration=\\"\S+\\"/) || false;
+                const durationArr = durationString ? durationString.toString().replace(/mediaPresentationDuration=\\"/, '').replace(/\\"/, '').trim().split(/\D/).slice(2, 5).map(time => parseInt(time)) : false;
+                
+                const duration = durationArr ? this.formatDuration({ hours: durationArr[0], minutes: durationArr[1], seconds: durationArr[2] + 1 }) : 'Live Stream';
+                const title = await page.evaluate(title => title.innerText.replace(/\s\|\sfacebook/i, ''), titleHandle);
                 const videoLink = await page.evaluate(meta => meta.getAttribute('content'), metaHandle);
 
-                // const dispatcher = connection.play(videoLink);
+                const data = {
+                    type: 'facebook',
+                    link: videoLink,
+                    title,
+                    duration,
+                    voiceChannel
+                };
 
-                // dispatcher.on('start', () => {
-                //     return message.reply('facebook video is playing!');
-                // });
+                message.guild.music.queue.push(data);
+
+                if (message.guild.music.isPlaying === false || message.guild.music.isPlaying === undefined) {
+                    message.guild.music.isPlaying = true;
+                    return this.play(message.guild.music.queue, message);
+                } else {
+                    return message.reply(`${data.title} added to queue`);
+                }
             } else if (query.match(/^(http(s)?:\/\/)?((w){3}\S)?\S+(\.)\S+\/\S+\.(\S){3}/)) {
-                const link = query.match(/^(http(s)?:\/\/)?((w){3}\S)?\S+(\.)\S+\/\S+\.(\S){3}/)[0];
+                // const link = query.match(/^(http(s)?:\/\/)?((w){3}\S)?\S+(\.)\S+\/\S+\.(\S){3}/)[0];
 
                 // const dispatcher = connection.play(link);
 
@@ -92,11 +107,29 @@ module.exports = class Play extends Command {
                 const videos = await youtube.searchVideos(query, 1);
                 if (!videos.length === 1) return message.reply('nothing found!');
 
-                // const dispatcher = connection.play(await ytdl(`https://www.youtube.com/watch?v=${videos[0].raw.id.videoId}`), { type: 'opus' });
+                const video = await youtube.getVideoByID(videos[0].raw.id.videoId);
 
-                // dispatcher.on('start', () => {
-                //     return message.reply('youtube video is playing!');
-                // });
+                const title = video.title;
+                const duration = this.formatDuration(video.duration);
+                const thumbnail = video.thumbnails.high.url;
+
+                const data = {
+                    type: 'youtube-search',
+                    link: `https://www.youtube.com/watch?v=${video.id}`,
+                    title,
+                    duration: duration !== '00:00:00' ? duration : 'Live Stream',
+                    thumbnail,
+                    voiceChannel
+                };
+
+                message.guild.music.queue.push(data);
+
+                if (message.guild.music.isPlaying === false || message.guild.music.isPlaying === undefined) {
+                    message.guild.music.isPlaying = true;
+                    return this.play(message.guild.music.queue, message);
+                } else {
+                    return message.reply(`${data.title} added to queue`);
+                }
             }
         } catch(err) {
             console.error(err);
@@ -108,38 +141,39 @@ module.exports = class Play extends Command {
         try {
             const voiceChannel = queue[0].voiceChannel;
             const connection = await voiceChannel.join();
+            let dispatcher;
 
-            // TODO change if to switch
-
-            if (queue[0].type === 'youtube') {
-                const dispatcher = connection.play(await ytdl(queue[0].link, { quality: 'highestaudio' }), { type: 'opus' });
-
-                dispatcher.on('start', () => {
-                    messsage.guild.music.dispatcher = dispatcher;
-                    messsage.guild.music.nowPlaying = queue[0];
-                    dispatcher.setVolume(messsage.guild.music.volume);
-                    messsage.reply(`${queue[0].title} is playing`);
-                    return queue.shift();
-                });
-
-                dispatcher.on('finish', () => {
-                    if (queue.length >= 1) return this.play(queue, messsage);
-                    else {
-                        messsage.guild.music.isPlaying = false;
-                        messsage.say('queue ended');
-                        return voiceChannel.leave();
-                    }
-                });
-
-                dispatcher.on('error', err => {
-                    messsage.guild.music.queue = [];
-                    messsage.guild.music.isPlaying = false;
-                    messsage.guild.music.nowPlaying = false;
-                    messsage.say('error occured');
-                    voiceChannel.leave();
-                    throw err;
-                });
+            switch (queue[0].type) {
+                case 'youtube': { dispatcher = connection.play(await ytdl(queue[0].link, { quality: 'highestaudio' }), { type: 'opus' }); break; }
+                case 'facebook': { dispatcher = connection.play(queue[0].link); break; }
+                case 'youtube-search': { dispatcher = connection.play(await ytdl(queue[0].link), { type: 'opus' }); break; }
             }
+
+            dispatcher.on('start', () => {
+                messsage.guild.music.dispatcher = dispatcher;
+                messsage.guild.music.nowPlaying = queue[0];
+                dispatcher.setVolume(messsage.guild.music.volume);
+                messsage.reply(`${queue[0].title} is playing`);
+                return queue.shift();
+            });
+
+            dispatcher.on('finish', () => {
+                if (queue.length >= 1) return this.play(queue, messsage);
+                else {
+                    messsage.guild.music.isPlaying = false;
+                    messsage.say('queue ended');
+                    return voiceChannel.leave();
+                }
+            });
+
+            dispatcher.on('error', err => {
+                messsage.guild.music.queue = [];
+                messsage.guild.music.isPlaying = false;
+                messsage.guild.music.nowPlaying = false;
+                messsage.say('error occured');
+                voiceChannel.leave();
+                throw err;
+            });
         } catch(err) {
             console.error(err);
             return message.reply('cannot play what you requested!');
@@ -147,8 +181,6 @@ module.exports = class Play extends Command {
     }
 
     formatDuration = durationObject => {
-        const duration = `${durationObject.hours < 10 ? '0' + durationObject.hours : durationObject.hours ? durationObject.hours : '00'}:${durationObject.minutes < 10 ? '0' + durationObject.minutes : durationObject.minutes ? durationObject.minutes : '00'}:${durationObject.seconds < 10 ? '0' + durationObject.seconds : durationObject.seconds ? durationObject.seconds : '00'}`;
-        
-        return duration;
+        return `${durationObject.hours < 10 ? '0' + durationObject.hours : durationObject.hours ? durationObject.hours : '00'}:${durationObject.minutes < 10 ? '0' + durationObject.minutes : durationObject.minutes ? durationObject.minutes : '00'}:${durationObject.seconds < 10 ? '0' + durationObject.seconds : durationObject.seconds ? durationObject.seconds : '00'}`;
     }
 }
