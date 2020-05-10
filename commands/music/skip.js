2c644502737458917de2c932f4146f2a30c2f70a
Hazem Krimi <krimihazem1@gmail.com>
2020-05-10 06:05:51 +0100

    fixed skip emoji

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -28,7 +28,7 @@ module.exports = class Play extends Command {
             }
             else {
                 message.guild.music.dispatcher.emit('finish');
-                const embed = new MessageEmbed().setColor('#000099').setTitle(':stop_button: Skipped current track');
+                const embed = new MessageEmbed().setColor('#000099').setTitle(':track_next: Skipped current track');
                 return await message.say({ embed });
             }
         } catch (err) {
